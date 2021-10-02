b50dacd498847ba07d06472c13374885f9948a03
teguhprawira1 <90823397+teguhprawira1@users.noreply.github.com>
2021-10-02 01:50:02 -0700

    tugas pertemuan 2

---
+++ app/Views/halaman/daftar-member.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>Daftar Member</title>
+    </head>
+    <body>
+        <h1>Hai... Kamu Mau Menjadi Member</h1>
+        <p1>Isikan data diri kamu dulu ya agar kami kenal dengan member kami</p1>
+    </body>
+</html>
