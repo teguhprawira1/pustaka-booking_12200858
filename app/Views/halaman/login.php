a8a6c82b3d03b4c901289ee6e8362d8317189cf7
teguhprawira1 <90823397+teguhprawira1@users.noreply.github.com>
2021-10-02 01:51:03 -0700

    tugas pertemuan 2

---
+++ app/Views/halaman/login.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>perpustakaan login</title>
+    </head>
+    <body>
+
+        <h1>Selamat datang di sistem informasi perpustakaan</h1>
+        <p>
+            ini adalah layanan perpustakaan digital masa kini
+            <a href="<?=base_url('/daftar-member')?>">klik daftar</a> untuk mendaftar menjadi member
+        </p>
+
+    </body>
+</html>
