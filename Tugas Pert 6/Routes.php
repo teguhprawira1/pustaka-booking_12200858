6c7d62eb204dfbfbad09a8d4e2fb348b5d16ed1f
teguhprawira1 <90823397+teguhprawira1@users.noreply.github.com>
2021-11-02 01:36:28 -0700

    Add files via upload

---
+++ Tugas Pert 6/Routes.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Config;
+
+// Create a new instance of our RouteCollection class.
+$routes = Services::routes();
+
+// Load the system's routing file first, so that the app and ENVIRONMENT
+// can override as needed.
+if (file_exists(SYSTEMPATH . 'Config/Routes.php')) {
+    require SYSTEMPATH . 'Config/Routes.php';
+}
+
+/*
+ * --------------------------------------------------------------------
+ * Router Setup
+ * --------------------------------------------------------------------
+ */
+$routes->setDefaultNamespace('App\Controllers');
+$routes->setDefaultController('Home');
+$routes->setDefaultMethod('index');
+$routes->setTranslateURIDashes(false);
+$routes->set404Override();
+$routes->setAutoRoute(true);
+
+/*
+ * --------------------------------------------------------------------
+ * Route Definitions
+ * --------------------------------------------------------------------
+ */
+
+// We get a performance increase by specifying the default
+// route since we don't have to scan directories.
+$routes->get('/', 'Home::index');
+$routes->get('/hello', 'SelamatDatang::hal_awal');
+$routes->post('/login', 'Login::cekLogin');
+$routes->get('/daftar-member','SelamatDatang::daftar_member');
+$routes->get('/beranda', 'SelamatDatang::hal_beranda', ['filter' => 'auth']);
+$routes->get('/login', 'SelamatDatang::beranda_login', ['filter' => 'autoin']);
+$routes->get('/logout', function () {
+    Services::session()->destroy();
+    return redirect()->to('/login');
+});
+
+$routes->get('/pengguna-list', 'PenggunaController_12200858::index', ['filter'=>'auth']);  
+$routes->get('/pengguna', 'PenggunaController_12200858::form', ['filter'=>'auth']);
+$routes->get('/pengguna/(:num)', 'PenggunaController_12200858::edit/$1', ['filter'=>'auth']);
+$routes->post('/pengguna', 'PenggunaController_12200858::simpan', ['filter'=>'auth']);  
+$routes->patch('/pengguna', 'PenggunaController_12200858::patch', ['filter'=>'auth']);
+$routes->delete('/pengguna', 'PenggunaController_12200858::delete', ['filter'=>'auth']);
+
+/*
+ * --------------------------------------------------------------------
+ * Additional Routing
+ * --------------------------------------------------------------------
+ *
+ * There will often be times that you need additional routing and you
+ * need it to be able to override any defaults in this file. Environment
+ * based routes is one such time. require() additional route files here
+ * to make that happen.
+ *
+ * You will have access to the $routes object within that file without
+ * needing to reload it.
+ */
+if (file_exists(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php')) {
+    require APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php';
+}
